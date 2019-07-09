237ac58277b85a495e1e1d9963d3c31566d733ad
DerekTond <strike1894@126.com>
2019-07-09 18:22:45 +0800

    将SQLite数据库改为MySQL

--- taskassignor/settings.py
+++ taskassignor/settings.py
@@ -2,15 +2,15 @@ import os, sys
 
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
-WIN = sys.platform.startswith('win')
-if WIN:  # 如果是 Windows 系统，使用三个斜线
-    prefix = 'sqlite:///'
-else:  # 否则使用四个斜线
-    prefix = 'sqlite:////'
+# WIN = sys.platform.startswith('win')
+# if WIN:  # 如果是 Windows 系统，使用三个斜线
+#     prefix = 'sqlite:///'
+# else:  # 否则使用四个斜线
+#     prefix = 'sqlite:////'
 
 class BaseConfig:
     SECRET_KEY = os.getenv('SECRET_KEY', 'f2e232sfewD')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', prefix + os.path.join(basedir, 'data.db'))
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:q1889233@localhost/assignor?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
@@ -22,9 +22,10 @@ class ProductionConfig(BaseConfig):
 
 
 class TestingConfig(BaseConfig):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
-    WTF_CSRF_ENABLED = False
+    pass
+    # TESTING = True
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+    # WTF_CSRF_ENABLED = False
 
 
 config = {
